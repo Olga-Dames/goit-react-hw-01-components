c8acfc11f5856214ad0753bddcc0851b790b05e6
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-03-03 09:22:58 +0200

    proptypes

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -22,3 +22,14 @@ export const TransactionHistory = ({items}) => {
   </tbody>
 </table>
 }
+
+TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.exact({
+          id: PropTypes.string.isRequired,
+          type: PropTypes.string.isRequired,
+          currency: PropTypes.string.isRequired,
+          amount: PropTypes.number.isRequired,
+        })
+      ),
+}
