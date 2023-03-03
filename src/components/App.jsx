115fe24bf8e0b7dca3e0b7cf81cacfa031845dc6
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-03-03 09:56:51 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,14 +11,9 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
         alignItems: 'center',
-        gap: 20,
-        fontSize: 40,
-        color: '#010101',
       }}
     >
       <Profile
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,35 +1,36 @@
 import PropTypes from 'prop-types';
 import css from './TransactionHistory.module.css';
 
-export const TransactionHistory = ({items}) => {
-    return <table className={css.transactionHistory}>
-  <thead className ={css.tableHead}>
-    <tr >
-      <th className={css.tableTitle}>Type</th>
-      <th className={css.tableTitle}>Amount</th>
-      <th className={css.tableTitle}>Currency</th>
-    </tr>
-  </thead>
-  <tbody className={css.tableBody}>
-    {items.map(({id, type, amount, currency}) => (
-      <tr className={css.tableRow} key={id}>
-      <td className={css.tableValue}>{type}</td>
-      <td className={css.tableValue}>{amount}</td>
-      <td className={css.tableValue}>{currency}</td>
-    </tr>
-    ))}
-    
-  </tbody>
-</table>
-}
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.tableHead}>
+        <tr>
+          <th className={css.tableTitle}>Type</th>
+          <th className={css.tableTitle}>Amount</th>
+          <th className={css.tableTitle}>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={css.tableRow} key={id}>
+            <td className={css.tableValue}>{type}</td>
+            <td className={css.tableValue}>{amount}</td>
+            <td className={css.tableValue}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.exact({
-          id: PropTypes.string.isRequired,
-          type: PropTypes.string.isRequired,
-          currency: PropTypes.string.isRequired,
-          amount: PropTypes.number.isRequired,
-        })
-      ),
-}
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+    })
+  ),
+};
